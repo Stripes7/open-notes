9ac3c60d18946e09c3abfc45685e97701eaf13cb
Nate Gibson <rgibsonn@gmail.com>
2026-07-03 17:18:45 -0400

    Working on load_notes function

--- README.md
+++ README.md
@@ -1,15 +1,26 @@
 # Open Notes
+A notes application that interfaces with an LLM for storage and retrieval of notes.
 
-## Features
+
+## Current Features
+Ask an LLM a question - the question and answer will be stored in the notes directory as a .md file with timestamp.
+
 
 ## Installation
 
+
 ## Configuration (.env)
+API_Route = path to ollama service
+MODEL = model you would like to run
+DIR_PATH = directory in which notes will be stored
 
 ## Running the application
 
+
 ## Project Structure
 
-## Roadmap
+
+## Planned Features
+
 
 ## License
--- open-notes.py
+++ open-notes.py
@@ -4,12 +4,25 @@ import requests
 from pathlib import Path
 from datetime import datetime
 
+# TO DO's
+# Add docstrings to each function
+# Create a generate timestamp function to be used in save notes and load notes (or maybe just create file?) or two separate functions
+
 def load_config():
+    # Get env variables
     load_dotenv()
     api_route = os.getenv("API_ROUTE")
     model = os.getenv("MODEL")
+    notes_dir = os.getenv("NOTES_DIR")
+    notes_repo_dir = os.getenv("NOTES_REPO_DIR")
 
-    return api_route, model
+    # Create required directories if not exist
+    notes_path = Path(notes_dir)
+    notes_path.mkdir(parents=True, exist_ok=True)
+    notes_repo_path = Path(notes_repo_dir)
+    notes_repo_path.mkdir(parents=True, exist_ok=True)
+
+    return api_route, model, notes_path, notes_repo_path
 
 def get_user_input():
     user_question = input("Type your note here: ")
@@ -45,20 +58,33 @@ def create_note(question, llm_response):
     
     return new_note
 
-def save_note(new_note):
-    dir_path = Path("notes")
-    dir_path.mkdir(parents=True, exist_ok=True)
+def save_note(new_note, notes_path):
     time_stamp = datetime.now().strftime("%Y_%m_%d %H-%M-%S")
     file_name = f"{time_stamp}.md"
-    file_path = (dir_path / file_name)
+    file_path = (notes_path / file_name)
 
     with open(file_path, "w") as file:
         file.write(new_note)
 
     print(f"\nNote saved successfully as {file_name}")
 
+def load_notes(notes_path, notes_repo_path):
+    """Takes a path object - iterates through files in the directory - copies contents of each file to a central repository file"""
+    # I only want to create a file once 
+    time_stamp = datetime.now().strftime("%Y_%m_%d %H-%M-%S")
+    file_name = f"Note_Repo_{time_stamp}.md"
+    file_path = (notes_path / file_name)
+
+    for note in notes_path:
+        with open(notes_repo_path, "w") as file:
+            file.write(note)
+    
+    print(f"All notes copied and saved successfully as {file_path}")
+
+
+
 def main():
-    api_route, model = load_config()
+    api_route, model, notes_path = load_config()
     user_question = get_user_input()
     payload = build_payload(model, user_question)
     data = call_llm(api_route, payload)
@@ -68,7 +94,7 @@ def main():
 
     llm_response = print_response(data)
     new_note = create_note(user_question, llm_response)
-    save_note(new_note)
+    save_note(new_note, notes_path)
    
 if __name__ == "__main__":
     main()
